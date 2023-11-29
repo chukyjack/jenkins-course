03d624283075fd0723d9628f9af623014ded9ba4
Chuka Agu <cagu@styleseat.com>
2023-11-29 00:04:20 -0600

    add python build

---
+++ job-dsl/pythondocker.groovy
@@ -0,0 +1,22 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/chukyjack/resume_ats_beater.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('chukyjack/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
