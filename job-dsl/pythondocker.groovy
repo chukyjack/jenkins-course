cb91e2a8e371c5facf6b157cdc5604e3fc7c05bf
Chuka Agu <cagu@styleseat.com>
2023-11-29 00:05:33 -0600

    add django docker repo

--- job-dsl/pythondocker.groovy
+++ job-dsl/pythondocker.groovy
@@ -10,7 +10,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('chukyjack/docker-nodejs-demo')
+            repositoryName('chukyjack/docker-django-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
